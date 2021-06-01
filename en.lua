646df90f8c9343ca5ebbc75a5278083847bb5d4f
falcon <85174903+falc0n2@users.noreply.github.com>
2021-06-01 21:28:45 +0200

    Create en.lua
    This is for weed only!

---
+++ en.lua
@@ -0,0 +1,10 @@
+Locales ['en'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'Pritisnite ~INPUT_CONTEXT~ da skupljas ~g~Kanabis~s~',
+  ['weed_inventoryfull'] = 'nemas više prostora vo inventar za da skupljas ~g~Kanabis~s~.',
+  ['weed_processprompt'] = 'Pritisnite ~INPUT_CONTEXT~ za da početi ~g~Preradu Kanabisa~s~.',
+  ['weed_processingstarted'] = 'obradi ~g~Kanabis~s~ u ~g~Marihuana~s~...',
+  ['weed_processingfull'] = 'obrada ~r~otkazana~s~ zbog punog inventara!',
+  ['weed_processingenough'] = 'moraš imati ~b2+~s~ ~g~Kanabisa~s~ za da pocnes sa obradivanje.',
+  ['weed_processed'] = 'tisi obrađio/la ~b~2~s~ ~g~Kanabisa~s~ vo ~b~1~s~ ~g~Marihuana~s~',
+  ['weed_processingtoofar'] = 'obrada je ~r~otkazana~s~ zbog napuštanja područja.',
